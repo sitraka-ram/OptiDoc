c24ea1fbc6181fbdcba4d29b1754bec20f7ccfa3
sitraka-ram <ramanantsoasitraka50@gmail.com>
2026-03-19 11:13:30 +0300

    sitraka 1

--- app/src/main/java/com/OptiDoc/app/SettingsActivity.java
+++ app/src/main/java/com/OptiDoc/app/SettingsActivity.java
@@ -1,15 +1,20 @@
 package com.OptiDoc.app;
 
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.Switch;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.preference.PreferenceFragmentCompat;
 
 
@@ -20,8 +25,16 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    private Switch switchNight;
+    private static final String PREFS = "prefs";
+    private static final String NIGHT_MODE = "night_mode";
+
+
+    private SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.settings_activity);
@@ -40,7 +53,21 @@ public class SettingsActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
+        /// Eto ny mode nuit
+
+        /// eto ny quitter
+        LinearLayout btnExit = findViewById(R.id.btnExit);
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishAffinity();
+
+                System.exit(0);
+            }
+        });
+
     }
+
 
 
 }
